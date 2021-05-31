0029313cc18b20adc52c70f293bda1e72a721bba
Dreamlone <mik_sar@mail.ru>
2021-05-31 13:51:45 +0300

    update models and names

--- models/interpolation/interpolation_alg.py
+++ models/interpolation/interpolation_alg.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from scipy import optimize
@@ -13,21 +12,27 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-def apply_on_dataset(meteo_df, stations_df, features_to_move, stations_ids,
-                     n_neighbors=2, knn_model=KNeighborsRegressor, save_path='file.csv'):
+def apply_on_dataset(meteo_df: pd.DataFrame, stations_df: pd.DataFrame,
+                     features_to_move: list, stations_ids: list,
+                     n_neighbors: int = 2, knn_model=KNeighborsRegressor, save_path='file.csv',
+                     vis_station_stage=False):
     """ Применяет функции к нужным датафремам
 
     :param meteo_df: датафрейм с метеоданными
     :param stations_df: датафрейм с данными уровней
     :param features_to_move: признаки, которые нужно перемещать с метеостанций в
     "гидропосты"
+    :param stations_ids: список идентефикаторов станций
     :param n_neighbors: количество соседей, по которым интерполируются значения
+    :param knn_model: алгоритм для расчета значений
+    :param save_path: куда требуется сохранить интерполированные данные
+    :param vis_station_stage: требуется ли отрисовывать графики уровней
     """
 
     # Файл с координатами гидропостов
-    hydro_coord = pd.read_csv('../first_data/track_2_package/hydro_coord.csv')
+    hydro_coord = pd.read_csv('../../first_data/track_2_package/hydro_coord.csv')
     # Файл с координатами метеостанций
-    meteo_coord = pd.read_csv('../first_data/track_2_package/meteo_coord.csv')
+    meteo_coord = pd.read_csv('../../first_data/track_2_package/meteo_coord.csv')
     meteo_coord['station_id_meteo'] = meteo_coord['station_id']
 
     dates = []
@@ -53,6 +58,14 @@ def apply_on_dataset(meteo_df, stations_df, features_to_move, stations_ids,
             start_date = min(station_df_local['date'])
             end_date = max(station_df_local['date'])
             all_dates = pd.date_range(start_date, end_date)
+
+            if vis_station_stage:
+                plt.plot(station_df_local['date'], station_df_local['stage_max_hydro'])
+                plt.xlabel('Дата', fontsize=15)
+                plt.ylabel('Максимальное значение уровня, см', fontsize=15)
+                plt.title(f'Hydro station id - {station_id}')
+                plt.show()
+
             for current_date in all_dates:
                 # Получаем объединенные данные для выбранного срока - один день
                 merged_current = merged[merged['date'] == current_date]
@@ -105,21 +118,25 @@ def apply_on_dataset(meteo_df, stations_df, features_to_move, stations_ids,
     new_station_info['date'] = dates
     new_station_info.to_csv(save_path, index=False)
 
+# Гидрологические посты для которых требуются данные
+# 3019, 3027, 3028, 3029, 3030, 3035, 3041, 3045, 3230, 3050
 
-meteo_df = pd.read_csv('../first_data/track_2_package/meteo_3hours.csv')
-meteo_df['date'] = pd.to_datetime(meteo_df['date_local'])
+# Параметры 3h - 'air_temperature', 'relative_humidity', 'pressure' - press
+# 'wind_direction', 'wind_speed_aver', 'precipitation' - wind
 
-stations_df = pd.read_csv('../first_data/track_2_package/train.csv')
+# Параметры 1day - 'snow_coverage_station', 'snow_height'
+
+meteo_df = pd.read_csv('../../first_data/track_2_package/meteo_1day.csv')
+meteo_df['date'] = pd.to_datetime(meteo_df['date'])
+
+stations_df = pd.read_csv('../../first_data/track_2_package/train.csv')
 stations_df['date'] = pd.to_datetime(stations_df['date'])
 
 apply_on_dataset(meteo_df=meteo_df,
                  stations_df=stations_df,
-                 features_to_move=['air_temperature', 'relative_humidity', 'pressure'],
+                 features_to_move=['snow_coverage_station', 'snow_height'],
                  knn_model=KNeighborsRegressor,
-                 n_neighbors=1,
-                 save_path='gap_meteo_3hour_int_3036_press.csv',
-                 stations_ids=[3036])
-
-# Гидрологические посты
-# 3019, 3027, 3028, 3030, 3035, 3041, 3045, 3230, 3050
-# 3029
+                 n_neighbors=3,
+                 save_path='gap_meteo_3036.csv',
+                 stations_ids=[3036],
+                 vis_station_stage=True)
